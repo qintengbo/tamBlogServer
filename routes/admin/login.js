28da6c742d909d69e9b9360055351f4c61dc8c57
qintengbo <qintb@hua-cloud.com.cn>
2018-09-18 00:50:28 +0800

    修改数据库地址为阿里云

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -8,13 +8,16 @@ router.post('/', function(req, res, next) {
 	var query = req.query;
 	MongoClient.connect(path, function(err, client) {
 	  if (err) throw err;
-	  var db = client.db('tamBlog');
+	  var db = client.db('test');
 	  db.collection('user').find().toArray(function(err, response) {
 	    if (err) throw err;
+	    console.log(response);
 	    if (response[0]['userName'] === query.userName && response[0]['password'] === query.password) {
         res.send({
           code: 0,
-          msg: '登录成功'
+          msg: '登录成功',
+          name: response[1]['name'],
+          age: response[1]['age']
         });
       } else {
 	      res.send({
