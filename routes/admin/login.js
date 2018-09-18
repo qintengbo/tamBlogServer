aa82571ece2dd057e8f0403e3314a90837cb3f41
qintengbo <qintb@hua-cloud.com.cn>
2018-09-19 01:04:45 +0800

    解决参数接收问题

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -5,19 +5,16 @@ var path = require('../dbPath');
 
 /* 登录接口 */
 router.post('/', function(req, res, next) {
-	var query = req.query;
+	var params = req.body;
 	MongoClient.connect(path, function(err, client) {
 	  if (err) throw err;
 	  var db = client.db('test');
 	  db.collection('user').find().toArray(function(err, response) {
 	    if (err) throw err;
-	    console.log(response);
-	    if (response[0]['userName'] === query.userName && response[0]['password'] === query.password) {
+	    if (response[0]['userName'] === params.userName && response[0]['password'] === params.password) {
         res.send({
           code: 0,
-          msg: '登录成功',
-          name: response[1]['name'],
-          age: response[1]['age']
+          msg: '登录成功'
         });
       } else {
 	      res.send({
