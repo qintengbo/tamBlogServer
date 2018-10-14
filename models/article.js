91bc7d5a6bbc2dca24273586dedee0101ce8be6d
qintengbo <qintb@hua-cloud.com.cn>
2018-10-15 02:29:14 +0800

    新增条件查询数据库

--- models/article.js
+++ models/article.js
@@ -19,7 +19,7 @@ const ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  status: { // 状态： 0-未发布，1-发布
+  status: { // 状态： 2-未发布，1-已发布
     type: Number,
     required: true
   },
--- routes/admin/article/articleList.js
+++ routes/admin/article/articleList.js
@@ -4,13 +4,26 @@ const Article = require('../../../models/article');
 
 // 查询文章列表接口
 router.get('/articleList', (req, res) => {
-  Article.find((err, collection) => {
+  console.log(req.query)
+  // 查询数据库条件
+  let params = {
+    title: { $regex: req.query.keyWord },
+    classification: req.query.classification === 'null' ? { $regex: '' } : req.query.classification,
+    tag: req.query.tag === 'null' ? { $regex: '' } : req.query.tag,
+    status: req.query.status === '0' ? { $gt: 0 } : Number(req.query.status),
+  };
+  // 判断是否有日期范围查询
+  if (req.query.date !== '') {
+    params.date = { $gte: req.query.date[0], $lte: req.query.date[1] };
+  }
+  Article.find(params, null, { sort: { _id: -1 } }, (err, collection) => {
     if (err) throw err;
     res.send({
       code: 0,
       msg: '获取文章列表成功',
       data: {
-        list: collection
+        list: collection,
+        total: collection.length
       }
     })
   });
