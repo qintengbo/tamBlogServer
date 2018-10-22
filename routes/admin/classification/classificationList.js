d59d611d28c9ffef968de6d6a2d4eaa7536a6fdb
qintb <qintb@hua-cloud.com.cn>
2018-10-22 17:47:15 +0800

    解决分类和标签被使用的文章数

--- routes/admin/classification/classificationList.js
+++ routes/admin/classification/classificationList.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Article = require('../../../models/article');
 const Classification = require('../../../models/classification');
 
 // 查询分类列表接口
 router.get('/classificationList', (req, res) => {
+  // 查询文章集合中分类引用次数
+  Article.aggregate([
+    { $group: { _id: '$classification', count: { $sum: 1 } } }
+  ]).then(doc => {
+    doc.forEach(item => {
+      // 更新文章数
+      Classification.updateOne({ _id: item._id }, { articleNum: item.count }, (err, raw) => {
+        if (err) throw err;
+      });
+    });
+  });
   Classification.find({ name: { $regex: req.query.keyWord } }, null, { sort: { date: -1 } }, (err, collection) => {
     if (err) throw err;
     res.send({
--- routes/admin/tag/tagList.js
+++ routes/admin/tag/tagList.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const Article = require('../../../models/article');
 const Tag = require('../../../models/tag');
 
 // 查询标签列表接口
 router.get('/tagList', (req, res) => {
+  // 查询文章集合中标签引用次数
+  Article.aggregate([
+    { $unwind: '$tag' },
+    { $group: { _id: '$tag', count: { $sum: 1 } } }
+  ]).then(doc => {
+    doc.forEach(item => {
+      // 更新文章数
+      Tag.updateOne({ _id: item._id }, { articleNum: item.count }, (err, raw) => {
+        if (err) throw err;
+      });
+    });
+  });
   Tag.find({ name: { $regex: req.query.keyWord } }, null, { sort: { date: -1 } }, (err, collection) => {
     if (err) throw err;
     res.send({
