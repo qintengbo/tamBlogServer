f6df1466fe872e4a017a96c07a734f0598ce0a47
qintb <qintb@hua-cloud.com.cn>
2019-01-07 16:55:43 +0800

    优化上传文件

--- routes/admin/common/uploadFile.js
+++ routes/admin/common/uploadFile.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     cb(null, 'public/tmp/');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null, file.originalname);
   }
 });
 // 文件上传设置
