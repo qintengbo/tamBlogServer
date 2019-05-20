4cd0020ae7c4c0fde516a9ddcdedb5928ae5d240
qintb <qintb@hua-cloud.com.cn>
2019-05-20 19:21:43 +0800

    优化代码

--- routes/front/article/articleList.js
+++ routes/front/article/articleList.js
@@ -4,14 +4,49 @@ const Article = require('../../../models/article');
 
 // 已上线文章列表接口
 router.get('/articleList', (req, res) => {
-  Article.find({ status: 1 }, null, { sort: { updateDate: -1 } }, (err, collection) => {
+  // 模糊查询
+  let params = {
+    title: { $regex: req.query.keyWord || '' },
+    status: 1
+  };
+  // 判断是否有日期范围查询
+  if (req.query.date && req.query.date !== '') {
+    params.updateDate = { $gte: req.query.date[0], $lte: req.query.date[1] };
+  }
+  // 判断是否有分类查询
+  if (req.query.classification && req.query.classification !== 'null') {
+    params.classification = req.query.classification;
+  }
+  // 判断是否有标签查询
+  if (req.query.tag && req.query.tag !== 'null') {
+    params.tag = req.query.tag;
+  }
+  // 查询数据总条数
+  let total;
+  Article.find(params).count((err, count) => {
     if (err) throw err;
-    res.send({
-      code: 0,
-      msg: '获取文章列表成功',
-      data: {
-        list: collection
-      }
+    total = count;
+    Article.find(params, null, { 
+      sort: { updateDate: -1 },
+      skip: (Number(req.query.page) - 1) * Number(req.query.size), 
+      limit: Number(req.query.size)
+    }, (err, collection) => {
+      if (err) throw err;
+      let opts = [
+        { path: 'classification', select: 'name' },
+        { path: 'tag', select: 'name' }
+      ];
+      Article.populate(collection, opts, (err, doc) => {
+        if (err) throw err;
+        res.send({
+          code: 0,
+          msg: '获取文章列表成功',
+          data: {
+            list: doc,
+            total: total
+          }
+        });
+      });
     });
   });
 });
