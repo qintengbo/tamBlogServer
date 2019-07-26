e840e91a19e7c35708a20d0c1454603cd68c77c1
qintb <qintb@hua-cloud.com.cn>
2019-07-26 18:17:17 +0800

    优化代码

--- app.js
+++ app.js
@@ -43,6 +43,18 @@ mongoose.set('useFindAndModify', false);
 // 连接数据库
 mongoose.Promise = global.Promise;
 mongoose.connect(config.dbPath, { useNewUrlParser: true });
+// 连接成功
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connection open to ' + config.dbPath);
+});
+// 连接失败
+mongoose.connection.on('error', err => {
+  console.log('Mongoose connection error: ' + err);
+});
+// 连接断开
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose connection disconnected');
+});
 
 // error handler
 app.use(function(err, req, res, next) {
