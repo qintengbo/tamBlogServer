202952f747b814810c4b10448314dec51064bd0e
qintb <qintb@hua-cloud.com.cn>
2020-12-16 18:40:23 +0800

    feat: 新增又拍云认证鉴权

---
+++ services/upyunAuth.js
@@ -0,0 +1,8 @@
+/*
+ * @Author       : qintengbo
+ * @Date         : 2020-12-16 17:32:05
+ * @Description  : 又拍云认证鉴权
+ */
+
+const crypto = require('crypto');
+const date = new Date().toUTCString();
