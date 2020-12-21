b37dca7b77f6e7287e45a635f39cf8d6e6756d7e
qintb <qintb@hua-cloud.com.cn>
2020-12-21 17:12:10 +0800

    style: 修改.gitignore文件

--- config/upyunConfig.js
+++
@@ -1,7 +0,0 @@
-// 又拍云配置文件
-module.exports = {
-  operator: 'admin', // 操作员
-  password: 'Ef9Joku5BIbZBKbdfgd7pgZKK4rYJPD1', // 密码 
-  bucket: 'tamblog-produce', // 存储区
-  url: 'http://tamblog-produce.test.upcdn.net', // 加速域名 
-}
